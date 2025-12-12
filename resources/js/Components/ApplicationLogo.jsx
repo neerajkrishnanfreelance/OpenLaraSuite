3f0ec37fd982c4976644b9814af49c39066090b3
Neeraj Krishnan V M <neeraj.krishnan@appingtechnology.com>
2025-12-12 12:07:00 +0530

    Added

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,5 +1,10 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+
 export default function ApplicationLogo(props) {
     return (
-<div></div>
+        <ArrowLeftIcon
+            className="w-6 h-6 text-white"
+            {...props}
+        />
     );
 }
